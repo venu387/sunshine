e696627b24eb55fdf3cfdb0e53d7ff10846331d6
Venu Srinivasa Reddy <venu387@users.noreply.github.com>
2021-07-20 15:20:03 -0400

    Add missing file

---
+++ Sunshine/src/app/assets/css/shared.tsx
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+export const sharedCss = StyleSheet.create({
+  heading: {
+    fontSize: 25,
+  },
+  bgImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
